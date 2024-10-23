4606356cffdb2812620541ce458323d8952b3446
rosasuffo <rositasuffo@gmail.com>
2024-10-23 23:58:08 +0200

    Todo (por ahora)

---
+++ Assets/Scripts/UI/UI_Text.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Text : UI_CustomComponent
+{
+    public UI_TextSO textData;
+    public Style style;
+    private TextMeshProUGUI textMeshProUGUI;
+
+    public override void Setup()
+    {
+        textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    public override void Configure()
+    {
+        textMeshProUGUI.color = textData.theme.GetTextColor(style); 
+        textMeshProUGUI.font = textData.font;
+        textMeshProUGUI.fontSize = textData.size;
+
+    }
+}
